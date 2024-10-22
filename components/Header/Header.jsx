2e19cee21228dd85a625a148c1c5aade3eee6f3f
Ihtisham Ul Haq <ahtshamsaleem51@gmail.com>
2024-10-22 19:04:48 +0500

    header services submenu add desktop

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -6,6 +6,8 @@ import Link from "next/link";
 import { HiOutlineSearch } from "react-icons/hi";
 
 import { usePathname } from "next/navigation";
+import { ControlledMenu, MenuItem, useHover, useMenuState } from "@szhsin/react-menu";
+import { useRef, useState } from "react";
 
 
 
@@ -24,13 +26,23 @@ const Header = () => {
     const pathname = usePathname(); 
 
 
+
+
+
+
+
+    const ref = useRef(null);
+    const [isOpen, setOpen] = useState(false);
+    const { anchorProps, hoverProps } = useHover(isOpen, setOpen);
+
+
    
 
 
 
 //fixed z-[9999]
   return (
-    <section className="w-full bg-gray-50 shadow-md shadow-black/25 ">
+    <section className="w-full bg-gray-50 shadow-md shadow-black/25 fixed z-[9999]">
 
 
 
@@ -44,21 +56,54 @@ const Header = () => {
         <div className="flex  ">
           <nav>
             <ul className="flex justify-center items-center gap-8 ">
-              <li className={`hover:text-primary   border-b-2 border-transparent hover:border-primary py-2 ${pathname === '/' ? 'active' : ''}`}>
-                <Link href={"/"}>Home</Link>
+              <Link  href={"/"} className={`hover:text-primary   border-b-2 border-transparent hover:border-primary py-2 ${pathname === '/' ? 'active' : ''}`}>
+                Home
+                
+              </Link>
+              
+              <Link href={"/about-us"} className={`hover:text-primary  border-b-2 border-transparent hover:border-primary py-2 ${pathname === '/about-us' ? 'active' : ''}`}>
+                About Us
+              </Link>
+
+
+
+
+              <li  ref={ref} {...anchorProps} className={`hover:text-primary cursor-pointer border-b-2 border-transparent hover:border-primary py-2 ${pathname === '/services' ? 'active' : ''}`}>
+                Services
+
+                <ControlledMenu
+                {...hoverProps}
+                state={isOpen ? 'open' : 'closed'}
+                anchorRef={ref}
+                onClose={() => setOpen(false)}
+              >
+                <div className="flex flex-col ">
+
+
+                <Link onClick={() => setOpen(false)} className='text-black bg-white py-3 px-8 font-poppins font-normal hover:text-white hover:bg-[#6C63FF] outline-none border-b  ' href={'/service/accounts'}>Accounts </Link>
+                <Link onClick={() => setOpen(false)} className='text-black bg-white py-3 px-8 font-poppins font-normal hover:text-white hover:bg-[#6C63FF] outline-none border-b ' href={'/service/bookkeeping'}>Bookkeeping</Link>
+                <Link onClick={() => setOpen(false)} className='text-black bg-white py-3 px-8 font-poppins font-normal hover:text-white hover:bg-[#6C63FF] outline-none border-b ' href={'/service/company-formation'}>Company Formation</Link>
+                <Link onClick={() => setOpen(false)} className='text-black bg-white py-3 px-8 font-poppins font-normal hover:text-white hover:bg-[#6C63FF] outline-none border-b ' href={'/service/corporation-tax'}>Corporation Tax</Link>
+                <Link onClick={() => setOpen(false)} className='text-black bg-white py-3 px-8 font-poppins font-normal hover:text-white hover:bg-[#6C63FF] outline-none border-b' href={'/service/payroll'}>Payroll</Link>
+                <Link onClick={() => setOpen(false)} className='text-black bg-white py-3 px-8 font-poppins font-normal hover:text-white hover:bg-[#6C63FF] outline-none border-b' href={'/service/self-assessment'}>Self Assessment</Link>
+                <Link onClick={() => setOpen(false)} className='text-black bg-white py-3 px-8 font-poppins font-normal hover:text-white hover:bg-[#6C63FF] outline-none border-b' href={'/service/vat-return'}>VAT Return</Link>
+                <Link onClick={() => setOpen(false)} className='text-black bg-white py-3 px-8 font-poppins font-normal hover:text-white hover:bg-[#6C63FF] outline-none'  href={'/services'}>View All Services</Link>
+                
+
+
+                </div>
+              </ControlledMenu>
               </li>
-              <li className={`hover:text-primary  border-b-2 border-transparent hover:border-primary py-2 ${pathname === '/about-us' ? 'active' : ''}`}>
-                <Link href={"/about-us"}>About Us</Link>
-              </li>
-              <li className={`hover:text-primary  border-b-2 border-transparent hover:border-primary py-2 ${pathname === '/services' ? 'active' : ''}`}>
-                <Link href={"/services"}>Services</Link>
-              </li>
-              <li className={`hover:text-primary  border-b-2 border-transparent hover:border-primary py-2 ${pathname === '/news-and-articles' ? 'active' : ''}`}>
-                <Link href={"/news-and-articles"}>News & Articles</Link>
-              </li>
-              <li className={`hover:text-primary  border-b-2 border-transparent hover:border-primary py-2 ${pathname === '/faqs' ? 'active' : ''}`}>
-                <Link href={"/faqs"}>FAQs</Link>
-              </li>
+
+
+
+
+              <Link href={"/news-and-articles"} className={`hover:text-primary  border-b-2 border-transparent hover:border-primary py-2 ${pathname === '/news-and-articles' ? 'active' : ''}`}>
+                News & Articles
+              </Link>
+              <Link href={"/faqs"} className={`hover:text-primary  border-b-2 border-transparent hover:border-primary py-2 ${pathname === '/faqs' ? 'active' : ''}`}>
+               FAQs
+              </Link>
             </ul>
           </nav>
         </div>
