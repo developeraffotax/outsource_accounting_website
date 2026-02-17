4877e694d13d767bcce1cd6ce4617a9c600a5876
Zuhran Yousaf <35305@students.riphah.edu.pk>
2026-02-17 16:36:09 +0500

    improve services spacing from sides px and improved spacing for mobile

--- src/components/home/Services/Services.jsx
+++ src/components/home/Services/Services.jsx
@@ -3,34 +3,37 @@ import { useState } from "react";
 import QuoteButton from "@/components/shared/buttons/QuoteButton.jsx";
 import Model from "@/components/shared/forms/Model.jsx";
 import QuoteForm from "@/components/shared/forms/QuoteForm.jsx";
+import Container from "@/components/wraper/Container";
 
 const Services = ({ heading, description, serviceCardsComponent }) => {
   const [isModelOpen, setisModelOpen] = useState(false);
 
   return (
-    <section
-      className="flex flex-col self-center items-center content-center justify-center text-black bg-white lg:my-12 mx-8 my-5 md:mx-24  "
-      id="services"
-    >
-      <div className="flex flex-col self-center items-center content-center justify-center align-middle text-black bg-white flex-wrap">
-        <h2 className="text-[2rem] font-semibold mb-4 w-full text-center text-[#4632DA] ">
-          {heading}
-        </h2>
-        <p className="text-[1rem] font-normal mb-8 w-full md:w-2/3 text-start md:text-center text-[#333333]">
-          {description}
-        </p>
-        {serviceCardsComponent}
-      </div>
-      <div className="mt-6">
-        <QuoteButton onClick={() => setisModelOpen(true)} />
-      </div>
-      <Model isOpen={isModelOpen} onClose={() => setisModelOpen(false)}>
-        <h2 className="text-2xl md:text-3xl font-bold mb-6 text-left text-gray-800">
-          Quick <span className="text-(--color-buttonBlue)">Quote</span>
-        </h2>
-        <QuoteForm onSuccess={() => setisModelOpen(false)} />
-      </Model>
-    </section>
+    <Container>
+      <section
+        className="flex flex-col self-center items-center content-center justify-center text-black bg-white"
+        id="services"
+      >
+        <div className="flex flex-col self-center items-center content-center justify-center align-middle text-black bg-white flex-wrap">
+          <h2 className="text-[2rem] font-semibold mb-4 w-full text-center text-[#4632DA] ">
+            {heading}
+          </h2>
+          <p className="text-[1rem] font-normal mb-8 w-full md:w-2/3 text-start md:text-center text-[#333333]">
+            {description}
+          </p>
+          {serviceCardsComponent}
+        </div>
+        <div className="mt-6">
+          <QuoteButton onClick={() => setisModelOpen(true)} />
+        </div>
+        <Model isOpen={isModelOpen} onClose={() => setisModelOpen(false)}>
+          <h2 className="text-2xl md:text-3xl font-bold mb-6 text-left text-gray-800">
+            Quick <span className="text-(--color-buttonBlue)">Quote</span>
+          </h2>
+          <QuoteForm onSuccess={() => setisModelOpen(false)} />
+        </Model>
+      </section>
+    </Container>
   );
 };
 
